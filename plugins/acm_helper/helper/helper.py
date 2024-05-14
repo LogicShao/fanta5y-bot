c255ea30c933d74b601d0a2e63e2c508a5567a5b
SwedishDoveCooker <85827122+SwedishDoveCooker@users.noreply.github.com>
2024-05-14 13:32:17 -0400

    Update helper.py

--- plugins/acm_helper/helper/helper.py
+++ plugins/acm_helper/helper/helper.py
@@ -5,9 +5,12 @@ class ACMHelper:
     def __init__(self, url: str='127.0.0.1', port: int=7890):
         # set codeforces helper
         self.codeforces_helper = CodeforcesHelper(url, port)
+        # set luogu helper
+        self.luogu_helper = LuoguHelper()
 
         self.helper_dict = {
             'codeforces': self.codeforces_helper
+            'luogu' : self.luogu_helper
         }
     
     def get_online_judge_accepted_submissions(self, username: str, online_judge: str) -> list:
