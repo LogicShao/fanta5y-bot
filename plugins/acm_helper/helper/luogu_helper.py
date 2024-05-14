638e1d550431b2d9c4382b3364c4f80d3e45bf69
SwedishDoveCooker <85827122+SwedishDoveCooker@users.noreply.github.com>
2024-05-14 13:35:22 -0400

    Update luogu_helper.py

--- plugins/acm_helper/helper/luogu_helper.py
+++ plugins/acm_helper/helper/luogu_helper.py
@@ -5,13 +5,16 @@ class LuoguHelper:
         # 因为国内网站所以直连
         pass
 
-    def get_passed_problem_count(self, uid: str) -> int:
-        # get the submission of the user
+    def get_data(self, uid: str) -> dict:
+        # get the general data of the user
         url: str = 'https://www.luogu.com.cn/user/{uid}?_contentOnly=1'.format(uid=uid)
         headers : dict = { 
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4331.0 Safari/537.36", 
         }
         response: requests.Response = requests.get(url , headers = headers)
         response.raise_for_status()
-        data = response.json()
-        return data['currentData']['user']['passedProblemCount']
+        return response.json()
+
+    def get_solved_problems(self, uid: str) -> list:
+        # extract the list of solved problems 
+        return self.get_data(uid)['currentData']['passedProblems']
