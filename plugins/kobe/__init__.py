8f571e2c47dd9a9ba0b513ec1386dd6d56780fd4
logicshao <1712207189@qq.com>
2024-05-17 22:19:41 +0800

    fix kobe plugin

--- plugins/kobe/__init__.py
+++ plugins/kobe/__init__.py
@@ -2,8 +2,9 @@ from nonebot import get_plugin_config
 from nonebot.plugin import PluginMetadata
 
 from .config import Config
-
 from nonebot import on_message
+
+import re
 
 # register the command
 kobe = on_message(
@@ -12,14 +13,24 @@ kobe = on_message(
 )
 
 # 将要匹配的字符串列表
-match_strings = ['老大','牢大','我没意见','我有意见','坠机','直升机','肘击','别肘','想我了吗','我回来了','孩子','what can i say','whatcanisay','manba','曼巴','想你了','24','闪电旋风劈','极空寒冰砍','天宇屠龙舞','泰山陨石坠','man']
+match_strings = ['老大', '牢大','我没意见','我有意见','坠机','直升机','肘击','别肘','想我了吗','我回来了','孩子','what can i say','whatcanisay','manba','曼巴','想你了','24','闪电旋风劈','极空寒冰砍','天宇屠龙舞','泰山陨石坠','man']
 
 @kobe.handle()
 async def handle_message(bot, event) -> None:
-    message = str(event.get_message()).lower()  # 获取消息内容
+    message = str(event.get_message())  # 获取消息内容
     username = event.sender.nickname
-    if any(word in message for word in match_strings):
-        infor_str = ("孩子们，特别是你 @{username}，这并不好笑").format(username=username)
+    # remove the [at:qq=xxxx] in the message
+    message = re.sub(r'\[CQ:at,qq=\d+\]', '@{username}'.format(username=username), message)  # Remove the [at:qq=xxxx] in the message
+    # remove the picture in the message
+    message = re.sub(r'\[CQ:image,file=[a-zA-Z0-9]+\.image\]', '', message)
+    # remove the face
+    message = re.sub(r'\[CQ:face,id=\d+\]', '', message)
+    # remove the emoji
+    message = re.sub(r'\[CQ:emoji,id=\d+\]', '', message)
+    message = message.lower()
+    matchedWords = list(filter(lambda x: x in message, match_strings))
+    if len(matchedWords) > 0:
+        infor_str = "检测到关键词 {matchedWords}。御坂想说：孩子们，特别是你 @{username}，这并不好笑".format(matchedWords=' '.join(matchedWords), username=username)
         await kobe.finish(infor_str)
 
 __plugin_meta__ = PluginMetadata(
