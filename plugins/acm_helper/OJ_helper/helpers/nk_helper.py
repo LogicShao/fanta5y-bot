fab080b3bdafb0195ca31f765671708933d043d1
logicshao <1712207189@qq.com>
2024-06-02 17:58:38 +0800

    修改牛客比赛的部分

--- plugins/acm_helper/OJ_helper/helpers/nk_helper.py
+++ plugins/acm_helper/OJ_helper/helpers/nk_helper.py
@@ -3,7 +3,7 @@ from .OJ_helper import UserInfo
 from .OJ_helper import ContestInfo
 
 from pyquery import PyQuery as pq
-from datetime import datetime, timedelta
+from datetime import datetime
 import requests
 
 
@@ -19,7 +19,7 @@ class NowCoderHelper(OJHelper):
         return response.text
 
     def gatgatInfo(self, uid: str) -> dict:
-        doc = pq(self.get_data(uid)) # pq是pyquery的一个函数，用于解析html文档
+        doc = pq(self.get_data(uid))  # pq是pyquery的一个函数，用于解析html文档
         data = {}  # 写完才发现data其实是字典...本来是按列表写的
         # data 格式 [username, rating, rating_rank, solvedProblems], 均为字符串
         data['username'] = doc(
@@ -52,30 +52,53 @@ class NowCoderHelper(OJHelper):
             rating_rank=int(info['rating_rank']),
             solvedProblems=int(info['solvedProblems']),
         )
-    
+
     def getApproachingContestsList(self) -> list[ContestInfo]:
         return [ContestInfo(error='暂时没有实现 NowCoder 的比赛查询功能。do! 御坂抱歉说。')]
 
-    def gechapoints(self, days: int) -> list:
-        doc1=pq(requests.get('https://ac.nowcoder.com/acm/contest/vip-index').text)
-        doc2=pq(requests.get('https://ac.nowcoder.com/acm/contest/vip-index?topCategoryFilter=14').text)
-        contests=[i.text() for i in [i for i in doc1('.platform-mod.js-current .platform-item-main .btn.btn-primary.btn-xxs').siblings().items()][:len([i for i in doc1('.platform-mod.js-current .platform-item-main .btn.btn-primary.btn-xxs').items()])]] + [i.text() for i in [i for i in doc2('.platform-mod.js-current .platform-item-main .btn.btn-primary.btn-xxs').siblings().items()][:len([i for i in doc2('.platform-mod.js-current .platform-item-main .btn.btn-primary.btn-xxs').items()])]]
-        times=[i.text() for i in [i for i in doc1('.platform-mod.js-current .match-time-icon').items()]]+[i.text() for i in [i for i in doc2('.platform-mod.js-current .match-time-icon').items()]]
-        data=[]
+    def gechapoints(self, days: int) -> list[ContestInfo]:
+        doc1 = pq(requests.get(
+            'https://ac.nowcoder.com/acm/contest/vip-index').text)
+        doc2 = pq(requests.get(
+            'https://ac.nowcoder.com/acm/contest/vip-index?topCategoryFilter=14').text)
+        contests = [i.text() for i in [i for i in doc1('.platform-mod.js-current .platform-item-main .btn.btn-primary.btn-xxs').siblings().items()][:len([i for i in doc1('.platform-mod.js-current .platform-item-main .btn.btn-primary.btn-xxs').items()])]] + \
+            [i.text() for i in [i for i in doc2('.platform-mod.js-current .platform-item-main .btn.btn-primary.btn-xxs').siblings().items()]
+             [:len([i for i in doc2('.platform-mod.js-current .platform-item-main .btn.btn-primary.btn-xxs').items()])]]
+        times = [i.text() for i in [i for i in doc1('.platform-mod.js-current .match-time-icon').items()]] + \
+            [i.text() for i in [i for i in doc2(
+                '.platform-mod.js-current .match-time-icon').items()]]
+        data: list[ContestInfo] = []
         for i in range(len(times)):
-            start_time = datetime.strptime(times[i].split('： ')[1].split(' 至 ')[0], '%Y-%m-%d %H:%M')
-            if 0 <= (start_time - datetime.now()).days <= days:
-                formatted_start_time = start_time.strftime('开始时间: %m 月 %d 日 %H:%M')
-                data.append([contests[i],formatted_start_time])
+            startInfo, endInfo = times[i].split(  # type: ignore
+                '： ')[1].split(' 至 ')[:2]
+            start_time: datetime = datetime.strptime(startInfo, '%Y-%m-%d %H:%M')
+            # 移除 endInfo 结尾的 "(时长:x小时)"
+            endInfo = endInfo.split(' (', 1)[0]
+            end_time: datetime = datetime.strptime(endInfo, '%Y-%m-%d %H:%M')
+            
+            dist: int = (start_time - datetime.now()).days
+            if dist < 0 or dist > days:
+                continue
+
+            data.append(ContestInfo(
+                oj_name='牛客',
+                contest_name=contests[i], # type: ignore
+                start_time=int(start_time.timestamp()),
+                end_time=int(end_time.timestamp())
+            ))
         return data
 
-    def getApproachingContestsInfo(self, days: int) -> UserInfo:
-        data=self.gechapoints(days)
+    def getApproachingContestsInfo(self, days: int = 10) -> str:
+        data = self.gechapoints(days)
         if len(data) == 0:
-            return '暂无即将开始的比赛。do! 御坂如是说。'
-        # cf 和 luogu 实现比赛的代码有一点区别, 这里我又自己瞎写了一种(
-        msg: str = 'nk {days} 天内即将开始的比赛有 {cnt} 场：\n'.format(days=days, cnt=len(data))
-        for contest in data:
-            msg += contest[0]+'\n'+'开始时间'+contest[1]+'\n'
-        msg-='\n'
+            return '暂时没有即将开始的比赛。do! 御坂如是说。'
+
+        msg = 'nk {days} 天内即将的比赛有 {cnt} 场：\n'.format(
+            days=days, cnt=len(data))
+        
+        msg += '\n'.join(map(str, data))
+        
+        # 移除最后一个换行符
+        if len(data) > 0 and msg[-1] == '\n':
+            msg = msg[:-1]
         return msg
